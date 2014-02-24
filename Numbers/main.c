c63b60431fa2313adf8f2c63be5fa7f8209f4902
Ken Swain <ken@kenswain.com>
2014-02-24 03:25:33 -0600

    Initial commit of the Numbers problem in the BNR book

---
+++ Numbers/main.c
@@ -0,0 +1,22 @@
+//
+//  main.c
+//  Numbers
+//
+//  Created by Ken Swain on 2/24/14.
+//  Copyright (c) 2014 Ken Swain. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(int argc, const char * argv[])
+{
+    double x = 1;
+    x = sin(x);
+    printf("The sine of 1 is %.3f\n", x);
+    
+    return 0;
+    
+}
+
